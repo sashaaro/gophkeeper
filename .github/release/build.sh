5ba6766be1c07b20a1c3470ac7af14b1591e1e51
Aleksandr Arofikin <sashaaro@gmail.com>
2024-10-24 15:02:02 +0300

    Публикация релиза

--- .github/release/build.sh
+++ .github/release/build.sh
@@ -18,3 +18,33 @@ tar -czf ./build/client-darwin-amd64.${APP_VERSION}.tar.gz ./build/client-darwin
 tar -czf ./build/client-windows-amd64.${APP_VERSION}.tar.gz ./build/client-windows-amd64.exe
 
 # TODO Нужно опубликовать релиз с этой версией и бинарями. См. https://docs.github.com/en/rest/releases/assets?apiVersion=2022-11-28#upload-a-release-asset"
+createRelease() {
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/sashaaro/gophkeeper/releases \
+    -d '{"tag_name":"${APP_VERSION}","target_commitish":"main","name":"${APP_VERSION}","body":"Description of the release","draft":false,"prerelease":false,"generate_release_notes":false}'
+}
+
+uploadAsset() {
+  local ID=$1
+  local FILENAME=$2
+  echo "Upload asset ${FILENAME} to release ${ID}"
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Content-Type: application/octet-stream" \
+    "https://uploads.github.com/repos/sashaaro/gophkeeper/releases/${ID}/assets?name=${FILENAME}" \
+    --data-binary "@./build/${FILENAME}"
+}
+
+echo "Create Release"
+RELEASE_ID=`createRelease | grep -oP '(?<="id": )([^,]*)'`
+echo "Created RELEASE_ID: ${RELEASE_ID}"
+uploadAsset $RELEASE_ID client-linux-amd64.${APP_VERSION}.tar.gz
+uploadAsset $RELEASE_ID client-darwin-amd64.${APP_VERSION}.tar.gz
+uploadAsset $RELEASE_ID client-windows-amd64.${APP_VERSION}.tar.gz
