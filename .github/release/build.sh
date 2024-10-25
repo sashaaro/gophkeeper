f3b2317d55e3493b73352c9b1a6941d1e194b613
Aleksandr Arofikin <sashaaro@gmail.com>
2024-10-25 22:03:11 +0300

    Интерполяция неверная

--- .github/release/build.sh
+++ .github/release/build.sh
@@ -25,7 +25,7 @@ createRelease() {
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/sashaaro/gophkeeper/releases \
-    -d '{"tag_name":"${APP_VERSION}","target_commitish":"main","name":"${APP_VERSION}","body":"Description of the release","draft":false,"prerelease":false,"generate_release_notes":false}'
+    -d "{\"tag_name\":\"${APP_VERSION}\",\"target_commitish\":\"main\",\"name\":\"${APP_VERSION}\",\"body\":\"Description of the release\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":false}"
 }
 
 uploadAsset() {
